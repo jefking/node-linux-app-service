52db7e9c120b589a88937724b86a7cb64f32aa10
Jef King <jefkin@microsoft.com>
2016-11-02 10:01:15 +0300

    boom, I have a server.

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var favicon = require('serve-favicon');
+var appInsights = require("applicationinsights");
+
+var app = express();
+app.use(favicon(__dirname + '/favicon.ico'));
+app.use(favicon(__dirname + '/fav.ico'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var expressPort = process.env.PORT || 8080;
+
+var router = express.Router();
+
+app.get('/', function (req, res) {
+	res.type('text/plain');
+	res.send("alive");
+});
+
+app.use('/', router);
+
+app.listen(expressPort);
